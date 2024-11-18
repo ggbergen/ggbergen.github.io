e2b79dd472493c022c865889bbd15135c6560718
Dan-Eric Næss <dan-eric.naess@live.no>
2024-11-18 21:41:12 +0100

    Trying some rulesheet thing

--- src/pages/spill/melee.tsx
+++ src/pages/spill/melee.tsx
@@ -1,75 +1,105 @@
+import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { useFullscreen } from 'ahooks';
-import { useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 
 const imgStyle =
   'aspect-video w-[45%] border border-solid border-teal-400 object-cover';
 
-const Rules = () => {
+const stages = [
+  {
+    name: "Yoshi's Story",
+    img: '/rules/melee/yoshisstory-ssbm.png',
+  },
+  {
+    name: 'Fountain of Dreams',
+    img: '/rules/melee/fountainofdreams-ssbm.png',
+  },
+  {
+    name: 'Battlefield',
+    img: '/rules/melee/battlefield-ssbm.jpg',
+  },
+  {
+    name: 'Final Destination',
+    img: '/rules/melee/finaldestination-ssbm.png',
+  },
+  {
+    name: 'Dream Land',
+    img: '/rules/melee/dreamland-ssbm.jpg',
+  },
+  {
+    name: 'Pokemon Stadium',
+    img: '/rules/melee/pokemonstadium-ssbm.png',
+    cn: 'bg-red-800',
+  },
+];
+
+const Rules = forwardRef<HTMLDivElement>(({}, ref) => {
+  return (
+    <>
+      <div
+        className="relative flex flex-col content-center justify-center bg-black"
+        ref={ref}
+      >
+        <div className="relative aspect-video">
+          <video
+            src="/rules/melee/melee.webm"
+            autoPlay
+            loop
+            muted
+            className="pointer-events-none absolute max-h-fit min-h-full min-w-full object-cover"
+          ></video>
+          <div className="relative flex h-full flex-col text-white">
+            <h1 className="text-center text-[]">Super Smash Bros. Melee</h1>
+            <h2 className="text-center text-2xl font-bold">
+              4 stocks * 8 minutes * items off
+            </h2>
+            <div className="flex flex-grow">
+              <div className="h-full flex-1">
+                <div className="flex flex-wrap content-center justify-center gap-4">
+                  {stages.map((stage) => {
+                    return (
+                      <div
+                        className={cn(
+                          'w-[45%] border border-solid border-teal-400 bg-blue-800',
+                          stage.cn || '',
+                        )}
+                      >
+                        <img
+                          src={stage.img}
+                          className="aspect-video w-full object-cover"
+                        ></img>
+                        <p>{stage.name}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <p>Counterpick</p>
+              </div>
+              <div className="flex-1">
+                <h2>Regler</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+});
+
+const Melee: GamePage = () => {
   const ref = useRef<HTMLDivElement>(null);
+
   const [
     isFullscreen,
     { enterFullscreen, exitFullscreen, toggleFullscreen, isEnabled },
   ] = useFullscreen(ref);
 
   return (
-    <div
-      className="relative flex flex-col content-center justify-center bg-black"
-      ref={ref}
-    >
-      <div className="relative aspect-video">
-        <video
-          src="/rules/melee.webm"
-          autoPlay
-          loop
-          muted
-          className="pointer-events-none absolute max-h-fit min-h-full min-w-full object-cover"
-        ></video>
-        <div className="relative flex h-full flex-col text-white">
-          <h1 className="text-center text-5xl">Super Smash Bros. Melee</h1>
-          <div className="flex flex-grow">
-            <div className="flex h-full flex-1 flex-wrap content-center justify-center gap-4">
-              <img
-                className={imgStyle}
-                src="/rules/melee/finaldestination-ssbm.png"
-              ></img>
-              <img
-                className={imgStyle}
-                src="/rules/melee/battlefield-ssbm.jpg"
-              ></img>
-              <img
-                className={imgStyle}
-                src="/rules/melee/yoshisstory-ssbm.png"
-              ></img>
-              <img
-                className={imgStyle}
-                src="/rules/melee/dreamland-ssbm.jpg"
-              ></img>
-              <img
-                className={imgStyle}
-                src="/rules/melee/fountainofdreams-ssbm.png"
-              ></img>
-              <img
-                className={cn(imgStyle, 'border- border-4')}
-                src="/rules/melee/pokemonstadium-ssbm.png"
-              ></img>
-            </div>
-            <div className="flex-1">
-              <button onClick={enterFullscreen}>Full</button>
-              <h2>Regler</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Melee: GamePage = () => {
-  return (
     <>
       <p>Melee</p>
-      <Rules></Rules>
+      <Button onClick={enterFullscreen}>Fullscreen</Button>
+      <Rules ref={ref}></Rules>
     </>
   );
 };
